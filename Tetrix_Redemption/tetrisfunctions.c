bc4f782115d6c3b879cc9d1177180b21905bacdf
Santiago-Carazo <santiago.carazo@alumnos.upm.es>
2022-05-27 14:33:06 +0200

    Corrección menu

--- Tetrix_Redemption/tetrisfunctions.c
+++ Tetrix_Redemption/tetrisfunctions.c
@@ -23,8 +23,11 @@ int menu()
 
             case 'c':
             case 'C':
-                printf("Para mover la pieza en horizontal utilizar las flechas laterales, para rotar utilizar la flecha superior, y para bajar utilizar la inferior\n");
-                break;
+                       printf("Para mover la pieza en horizontal utilizar las flechas laterales,'a' o 'd'\n"
+                       "para rotar a la derecha la flecha superior o 'g'y para rotar a la izquierda 'f'\n"
+                       "para bajar utilizar la flecha inferior o 's' y para bajar instantaneo 'espacio'\n"
+                       "para guardar pieza utilizar 'k'\n");
+            break;
 
             case 'j':
             case 'J':
